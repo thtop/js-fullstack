24460bf1223c250b4171548615af606b5f8095a8
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-09 11:49:16 +0700

    finished day 2

---
+++ src/2.js
@@ -0,0 +1,21 @@
+console.log('Start....');
+
+// Functions -> verb
+
+//alert('Hi!');
+
+function greet(name, age) {
+    alert(`Hello, my name is ${name} I am ${age} years old`);
+}
+
+// greet('Lexa', 19);
+// greet('Clarke', 18);
+
+function triple(number) {
+    return 3 * number;
+}
+
+console.log(triple(3)); // 9
+
+
+console.log('End....');
