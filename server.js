fb15b2b51d6e4856fb414d1376c1c5d3aedb353a
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-11 04:58:36 +0700

    finished reading data from a database

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ mongodb.connect(connectionString, {useNewUrlParser: true}, function(err, client)
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
+
+  db.collection('items').find().toArray(function(err, items) {
+    //console.log(items);
     res.send(`<!DOCTYPE html>
     <html>
     <head>
@@ -35,38 +38,29 @@ app.get('/', function(req, res) {
         </div>
         
         <ul class="list-group pb-5">
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #1</span>
+          ${items.map(function(item) {
+            return  ` <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+            <span class="item-text">${item.text}</span>
             <div>
               <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
               <button class="delete-me btn btn-danger btn-sm">Delete</button>
             </div>
-          </li>
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #2</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
-          <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-            <span class="item-text">Fake example item #3</span>
-            <div>
-              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button class="delete-me btn btn-danger btn-sm">Delete</button>
-            </div>
-          </li>
+          </li>`;
+          }).join('')}
+        </div>
+      </li>
         </ul>
         
       </div>
       
     </body>
     </html>`);
+  });
 });
 
 app.post('/create-item', function(req,res) {
     db.collection('items').insertOne({text: req.body.item}, function() {
-      res.send('Thanks for submitting the form.');
+      res.redirect('/');
     });
 });
 
