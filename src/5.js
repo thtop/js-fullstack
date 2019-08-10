bc71b9e2e80e1f6c4595ea8db9dc413da345a3e4
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-10 09:58:41 +0700

    finished day 5

---
+++ src/5.js
@@ -0,0 +1,25 @@
+console.log('Start...');
+
+/**
+ * Making Decisions
+ */
+let strawberryCount = 5;
+
+if (strawberryCount > 9) {
+    document.write('Congrats!');
+} else {
+    document.write('Sorry, we do not ship orders that small.');
+}
+
+
+/**
+ * while loop
+ */
+let count = 2;
+while(count <= 10) {
+    document.write(`There are ${count} strawberries. <br>`);
+    count++;
+}
+
+
+console.log('End...');
