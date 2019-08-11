4284f18315b2ac030da5ca636bed9726eb6c4996
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-12 05:29:43 +0700

    What Is A Model?

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 exports.login = function() {
 
 }
@@ -7,6 +9,9 @@ exports.logout = function() {
 }
 
 exports.register = function(req, res) {
+    //console.log(req.body);
+    let user = new User(req.body);
+    user.register();
     res.send('Thanks for trying to register');
 }
 
---
+++ models/User.js
@@ -0,0 +1,9 @@
+let User = function(data) {
+    this.data = data;
+}
+
+User.prototype.register = function() {
+    // Validation
+}
+
+module.exports = User;
