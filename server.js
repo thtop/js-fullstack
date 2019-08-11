b6844a546c93563f6bbbbcb795c29fd55d00bbd1
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-11 10:15:00 +0700

    fixed delete feature

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/', function(req, res) {
             <span class="item-text">${item.text}</span>
             <div>
               <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-              <button date-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+              <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
             </div>
           </li>`;
           }).join('')}
