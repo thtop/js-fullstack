255570e130bd5e0d5d9f5c884da227f5ab59ef00
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-11 15:35:22 +0700

    Finished Client-Side Rendering

--- public/js/browser.js
+++ public/js/browser.js
@@ -8,6 +8,13 @@ function itemTemplate(item) {
   </li>`;
 }
 
+// Initial Page Load Render
+let ourHTML = items.map(function(item) {
+    return itemTemplate(item);
+}).join('');
+
+document.getElementById('item-list').insertAdjacentHTML('beforeend', ourHTML);
+
 // Create Feature
 let createField = document.getElementById('create-field');
 
