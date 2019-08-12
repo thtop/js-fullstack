27adeeb26ffe06a6988377c77766c90d71b92656
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-12 12:50:50 +0700

    Letting Users Log In

--- models/User.js
+++ models/User.js
@@ -62,6 +62,17 @@ User.prototype.validate = function() {
     }
 }
 
+User.prototype.login = function(callback) {
+    this.cleanUp();
+    usersCollection.findOne({username: this.data.username}, (err, attemptedUser) => {
+        if (attemptedUser && attemptedUser.password == this.data.password) {
+            callback('Congrats!');
+        } else {
+            callback('Invalid username / password');
+        }
+    });
+}
+
 User.prototype.register = function(req, res) {
     // Step #1: Validate user data
     this.cleanUp();
