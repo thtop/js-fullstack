250cd22ee3e3d09234a5351f891487661f913281
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-13 21:17:45 +0700

    The Edit Screen for a Post

--- controllers/postController.js
+++ controllers/postController.js
@@ -22,3 +22,12 @@ exports.viewSingle = async function (req, res) {
     res.render('404');
   }
 }
+
+exports.viewEditScreen = async function (req, res) {
+  try {
+    let post = await Post.findSingleById(req.params.id);
+    res.render('edit-post', {post: post});
+  } catch {
+    res.render('404');
+  }
+}
