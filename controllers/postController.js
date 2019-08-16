96e5966ba91d8ff32fff2bb85a131f3f309ac58c
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-16 08:07:47 +0700

    Let Users Delete a Post

--- controllers/postController.js
+++ controllers/postController.js
@@ -24,14 +24,28 @@ exports.viewSingle = async function (req, res) {
   }
 }
 
-exports.viewEditScreen = async function (req, res) {
+// exports.viewEditScreen = async function (req, res) {
+//   try {
+//     let post = await Post.findSingleById(req.params.id)
+//     if (post.authorId == req.visitorId) {
+//       res.render("edit-post", { post: post })
+//     } else {
+//       req.flash('errors', 'You do not have premission to perform that action.');
+//       req.session.save(() => res.redirect('/'));
+//     }
+//   } catch {
+//     res.render("404")
+//   }
+// }
+
+exports.viewEditScreen = async function(req, res) {
   try {
-    let post = await Post.findSingleById(req.params.id)
-    if (post.authorId == req.visitorId) {
-      res.render("edit-post", { post: post })
+    let post = await Post.findSingleById(req.params.id, req.visitorId)
+    if (post.isVisitorOwner) {
+      res.render("edit-post", {post: post})
     } else {
-      req.flash('errors', 'You do not have premission to perform that action.');
-      req.session.save(() => res.redirect('/'));
+      req.flash("errors", "You do not have permission to perform that action.")
+      req.session.save(() => res.redirect("/"))
     }
   } catch {
     res.render("404")
@@ -64,5 +78,15 @@ exports.edit = function (req, res) {
     req.session.save(function () {
       res.redirect("/")
     })
+  })
+}
+
+exports.delete = function(req, res) {
+  Post.delete(req.params.id, req.visitorId).then(() => {
+    req.flash('success', 'Post successfully deleted.');
+    req.session.save(() => res.redirect(`/profile/${req.session.user.username}`))
+  }).catch(() => {
+    req.flash('errors', 'You do not have pernission to perform that action.')
+    req.session.save(() => res.redirect('/'))
   })
 }
